911fee47a94681534bc085865c81d78dac46c6b3
Lena Levenets <63717025+sunswan@users.noreply.github.com>
2022-08-04 14:56:43 +0200

    js

--- js/map.js
+++ js/map.js
@@ -1,142 +1,139 @@
-
 function initMap() {
-    let myMap = { lat: 40.776597, lng: -73.952340 };
+    let myMap = { lat: 40.776597, lng: -73.95234 };
     let opt = {
         zoom: 15,
-        disableDefaultUI:true,    
+        disableDefaultUI: true,
 
         center: myMap,
         styles: [
             {
-                "featureType": "water",
-                "stylers": [
+                featureType: "water",
+                stylers: [
                     {
-                        "color": "#0e171d"
-                    }
-                ]
+                        color: "#0e171d",
+                    },
+                ],
             },
             {
-                "featureType": "landscape",
-                "stylers": [
+                featureType: "landscape",
+                stylers: [
                     {
-                        "color": "#1e303d"
-                    }
-                ]
+                        color: "#1e303d",
+                    },
+                ],
             },
             {
-                "featureType": "road",
-                "stylers": [
+                featureType: "road",
+                stylers: [
                     {
-                        "color": "#1e303d"
-                    }
-                ]
+                        color: "#1e303d",
+                    },
+                ],
             },
             {
-                "featureType": "poi.park",
-                "stylers": [
+                featureType: "poi.park",
+                stylers: [
                     {
-                        "color": "#1e303d"
-                    }
-                ]
+                        color: "#1e303d",
+                    },
+                ],
             },
             {
-                "featureType": "transit",
-                "stylers": [
+                featureType: "transit",
+                stylers: [
                     {
-                        "color": "#182731"
+                        color: "#182731",
                     },
                     {
-                        "visibility": "simplified"
-                    }
-                ]
+                        visibility: "simplified",
+                    },
+                ],
             },
             {
-                "featureType": "poi",
-                "elementType": "labels.icon",
-                "stylers": [
+                featureType: "poi",
+                elementType: "labels.icon",
+                stylers: [
                     {
-                        "color": "#f0c514"
+                        color: "#f0c514",
                     },
                     {
-                        "visibility": "off"
-                    }
-                ]
+                        visibility: "off",
+                    },
+                ],
             },
             {
-                "featureType": "poi",
-                "elementType": "labels.text.stroke",
-                "stylers": [
+                featureType: "poi",
+                elementType: "labels.text.stroke",
+                stylers: [
                     {
-                        "color": "#1e303d"
+                        color: "#1e303d",
                     },
                     {
-                        "visibility": "off"
-                    }
-                ]
+                        visibility: "off",
+                    },
+                ],
             },
             {
-                "featureType": "transit",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                featureType: "transit",
+                elementType: "labels.text.fill",
+                stylers: [
                     {
-                        "color": "#e77e24"
+                        color: "#e77e24",
                     },
                     {
-                        "visibility": "off"
-                    }
-                ]
+                        visibility: "off",
+                    },
+                ],
             },
             {
-                "featureType": "road",
-                "elementType": "labels.text.fill",
-                "stylers": [
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [
                     {
-                        "color": "#94a5a6"
-                    }
-                ]
+                        color: "#94a5a6",
+                    },
+                ],
             },
             {
-                "featureType": "administrative",
-                "elementType": "labels",
-                "stylers": [
+                featureType: "administrative",
+                elementType: "labels",
+                stylers: [
                     {
-                        "visibility": "simplified"
+                        visibility: "simplified",
                     },
                     {
-                        "color": "#e84c3c"
-                    }
-                ]
+                        color: "#e84c3c",
+                    },
+                ],
             },
             {
-                "featureType": "poi",
-                "stylers": [
+                featureType: "poi",
+                stylers: [
                     {
-                        "color": "#e84c3c"
+                        color: "#e84c3c",
                     },
                     {
-                        "visibility": "off"
-                    }
-                ]
-            }
-        ]
+                        visibility: "off",
+                    },
+                ],
+            },
+        ],
     };
 
-    let map = new google.maps.Map(
-        document.getElementById('map'), opt);
+    let map = new google.maps.Map(document.getElementById("map"), opt);
 
     let marker = new google.maps.Marker({
         position: myMap,
         map: map,
-        icon: "../img/marker.png"
+        icon: "../img/marker.png",
     });
 
-
     let infowindow = new google.maps.InfoWindow({
-        content: "<h3>91 Nolan Extensions Suite 670</h3><br/><p>+001 356-868-2454 </p>"
+        content:
+            "<h3>91 Nolan Extensions Suite 670</h3><br/><p>+001 356-868-2454 </p>",
     });
 
     marker.addListener("click", function () {
         infowindow.open(map, marker);
-    })
-
+    });
 }
--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
 // console.clear();
 $(document).ready(function () {
-    $('.slider').slick({
-        slidesToShow: 3,
-        adaptiveHeight: true,
-        arrows: false,
-        vertical: true,
-        autoplay:true,
-        autoplaySpeed: 3000,
-        verticalSwiping: true,
-        speed: 1000,
-        easing: `ease`,
-        infinite: true,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        responsive: [
-            {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
-          }
-           },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-          }
-    
-            }
+  $('.slider').slick({
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    arrows: false,
+    vertical: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    verticalSwiping: true,
+    speed: 1000,
+    easing: `ease`,
+    infinite: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+
+      }
     ]
-    });
+  });
 
 });
 
-                        
 
-$(function() {
-    $('.menu-btn').click(function() {
-        $('#navy').slideToggle("500, linear");
-        $('body').toggleClass('overlay');
-    });
+$(function () {
+  $('.menu-btn').click(function () {
+    $('#navy').slideToggle("500, linear");
+    $('body').toggleClass('overlay');
+  });
 });
